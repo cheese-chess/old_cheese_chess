1764558b992dfb2af09032949d211813be25035e
segfaultdev <segfaultdev@protonmail.com>
2021-05-31 18:31:17 +0200

    Added black pawn version of move check

--- chess.c
+++ chess.c
@@ -12,31 +12,50 @@ int chess_turn(char piece) {
 int chess_valid(char *board, int *move, int turn) {
   if (move[0] == move[1]) return 0;
   else if (chess_turn(board[move[0]]) == chess_turn(board[move[1]])) return 0;
+  else if (chess_turn(board[move[0]]) != turn) return 0;
 
   // Check if the move from piece at move[0] to index move[1] is valid
   // for the specified player(CHESS_WHITE or CHESS_BLACK)
 
-if (board[move[0]] == 'P') {
-  if (board[move[1]] == ' ') {
-    if (move[1] == move[0] - 8) {
-      return 1;
-    } else if (move[0] / 8 == 6 && move[1] == move[0] - 16) {
-      return 1;
+  if (board[move[0]] == 'P') {
+    if (board[move[1]] == ' ') {
+      if (move[1] == move[0] - 8) {
+        return 1;
+      } else if (move[0] / 8 == 6 && move[1] == move[0] - 16) {
+        return 1;
+      }
     }
-  } 
-  if (chess_turn(board[move[0]] - 7) == CHESS_BLACK || chess_turn(board[move[0]] - 9 == CHESS_BLACK)) {
-    if (move[1] == move[0] - 7 || move[1] == move[0] - 9) {
-      return 1;
+    if (chess_turn(board[move[0]] - 7) == CHESS_BLACK || chess_turn(board[move[0]] - 9 == CHESS_BLACK)) {
+      if (move[1] == move[0] - 7 || move[1] == move[0] - 9) {
+        return 1;
+      }
     }
   }
-}
+
+  if (board[move[0]] == 'p') {
+    if (board[move[1]] == ' ') {
+      if (move[1] == move[0] + 8) {
+        return 1;
+      } else if (move[0] / 8 == 1 && move[1] == move[0] + 16) {
+        return 1;
+      }
+    }
+    if (chess_turn(board[move[0]] + 7) == CHESS_WHITE || chess_turn(board[move[0]] + 9 == CHESS_WHITE)) {
+      if (move[1] == move[0] + 7 || move[1] == move[0] + 9) {
+        return 1;
+      }
+    }
+  }
 
   return 0;
 }
 
 int **chess_get_moves(char *board, int turn) {
   // Get a list of moves for each piece of the specified player, checking
-  // them with chess_valid() before
+  // them with chess_valid() before. It's NULL terminated
+
+  // An example list would look like:
+  // {{0, 1}, {2, 3}, {4, 5}, NULL}
   return NULL;
 }
 
